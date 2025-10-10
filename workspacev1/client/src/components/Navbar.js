13be4e0eaa427efb6412be94a589a789067d57b2
Angelo Carlotto <angelo.carlotto@dayforce.com>
2025-10-10 10:54:30 -0400

    improved logout button

--- workspacev1/client/src/components/Navbar.js
+++ workspacev1/client/src/components/Navbar.js
@@ -16,7 +16,10 @@ function Navbar() {
       <div className="navbar-brand">
         <h1>TaskFlow</h1>
       </div>
-      
+      {/* Debug info: show user and auth state */}
+      <div style={{ color: 'red', fontSize: '0.8em' }}>
+        DEBUG: user = {JSON.stringify(user)}
+      </div>
       <div className="navbar-content">
         {notifications.length > 0 && (
           <div className="notifications">
@@ -25,9 +28,9 @@ function Navbar() {
           </div>
         )}
         
-        <div className="user-info">
+        <div className="user-info" style={{ border: '2px solid red', background: '#fffbe6', padding: '4px', borderRadius: '6px' }}>
           <span>Welcome, {user?.username}</span>
-          <button className="btn btn-link" onClick={handleLogout}>
+          <button className="btn btn-link" style={{ border: '2px solid blue', background: '#e6f7ff', marginLeft: '8px' }} onClick={handleLogout}>
             Logout
           </button>
         </div>
